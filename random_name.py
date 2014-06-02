5dddd6ed503326e421d8262d915f4e3e68318a3b
Yasha Mostofi <yasha.mostofi@gmail.com>
2014-06-02 09:55:03 -0500

    yay

--- random_name.py
+++ random_name.py
@@ -1,1 +1,7 @@
-print "hi"
+import random
+names_list = ["Yasha", "Kireem", "Andrew", "Nishaad", "Amy", "Davis"]
+randIndex = random.randint(len(names_list))
+print randIndex
+randName = names_list[randIndex]
+print randName
+names_list.remove(randName)
